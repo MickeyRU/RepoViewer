88c5211b12ce075910e6715f556bd6c866ec9c77
Pavel Afanasev <afanasyevpv9@gmail.com>
2024-11-29 14:02:06 +0300

    Initial Commit

---
+++ RepoViewer/RepoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoViewerApp.swift
+//  RepoViewer
+//
+//  Created by Pavel Afanasev on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RepoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
